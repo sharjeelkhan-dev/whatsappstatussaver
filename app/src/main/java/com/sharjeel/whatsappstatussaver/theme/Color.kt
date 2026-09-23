accfcc0c8a07cb2606af899c5e519ec1ceb8a672
Sharjeel Khan <sharjeelkhan111213@gmail.com>
2026-09-23 12:29:57 +0500

    Newly Update 23/09/2026

--- app/src/main/java/com/sharjeel/whatsappstatussaver/theme/Color.kt
+++ app/src/main/java/com/sharjeel/whatsappstatussaver/theme/Color.kt
@@ -2,6 +2,20 @@ package com.sharjeel.whatsappstatussaver.theme
 
 import androidx.compose.ui.graphics.Color
 
+val PrimaryGreen = Color(0xFF00A884)
+val SecondaryGreen = Color(0xFF005E4C)
+val SoftGreen = Color(0xFFE7FFFA)
+
+val DarkBackground = Color(0xFF111B21)
+val DarkSurface = Color(0xFF1F2C34)
+val DarkSurfaceVariant = Color(0xFF2A3942)
+val DarkTextPrimary = Color(0xFFE9EDEF)
+val DarkTextSecondary = Color(0xFF8696A0)
+
+val LightBackground = Color(0xFFFBFDFF)
+val LightSurface = Color(0xFFFFFFFF)
+val LightTextPrimary = Color(0xFF1C2D2A)
+
 val Teal80 = Color(0xFF4DB6AC)
 val TealGrey80 = Color(0xFFA0C0BB)
 val LightTeal80 = Color(0xFF80CBC4)
@@ -9,5 +23,6 @@ val LightTeal80 = Color(0xFF80CBC4)
 val Teal40 = Color(0xFF00897B)
 val TealGrey40 = Color(0xFF5A7571)
 val LightTeal40 = Color(0xFF00695C)
+
 
 
